bd1d15845dd0c11a0d1b19e7eab65c8785b3360e
botnet <kayzeebiz@gmail.com>
2020-12-03 20:40:35 +0100

    fixes

--- fullblog/settings.py
+++ fullblog/settings.py
@@ -127,15 +127,27 @@ SOCIALACCOUNT_PROVIDERS = {
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'forum',
+#         'USER': 'postgres',
+#         'PASSWORD': 'fame007dav',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'forum',
-        'USER': 'postgres',
-        'PASSWORD': 'fame007dav',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.mysql',
+        # "ENGINE":'mysql.connector.django',
+        'NAME': "blog",
+        'USER': 'root',
+        'PASSWORD': 'moshkay55',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
